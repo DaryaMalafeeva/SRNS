87e6b06687724b512d071346b0dbe204a3859a3c
Darya Malafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2020-05-05 23:26:38 +0300

    Update main_w3.py

--- main_w3.py
+++ main_w3.py
@@ -27,22 +27,22 @@ G_E1_B_list_str  = list(format(int(read_codes.G_E1_B_16, 16), '4092b'))
 G_E1_B_list_int  = [int(x) for x in G_E1_B_list_str]
 G_E1_B_array     = np.array((G_E1_B_list_int))
 # повторение элементов ДК на время моделирования
-G_E1_B_full      = numpy.matlib.repmat(G_E1_B_array, 1, int(mod_time /T_dk))
+G_E1_B_full      = np.transpose(numpy.matlib.repmat(G_E1_B_array, 1, int(mod_time /T_dk)))
+# согласовываем чипы последовательностей
+G_E1_B           = np.repeat(G_E1_B_full, math.ceil(amount_k/ len(G_E1_B_full)))
+
 
 G_E1_C_list_str  = list(format(int(read_codes.G_E1_C_16, 16), '4092b'))
 G_E1_C_list_int  = [int(x) for x in G_E1_C_list_str]
-G_E1_C_array     = np.array((G_E1_C_list_int))
-G_E1_C_full      = numpy.matlib.repmat(G_E1_C_array, 1, int(mod_time /T_dk))
-
-
-
-#G_E1_C = np.repeat(G_E1_C_full, math.ceil(amount_k/ len(G_E1_C_full)))
+G_E1_C_array     = np.transpose(np.array((G_E1_C_list_int)))
+G_E1_C_full      = np.transpose(np.matlib.repmat(G_E1_C_array,1, int(mod_time /T_dk)))
+G_E1_C           = np.repeat(G_E1_C_full, math.ceil(amount_k/ len(G_E1_C_full)))
 
 
 """"---------------------------Оверлейный код-------------------------------"""
 # параметры оверлейного кода
-T_ok   = 100 * 1e-3 # период ОК
-tau_ok = 4 * 1e-3   # длительность элементарного символа ОК
+T_ok             = 100 * 1e-3 # период ОК
+tau_ok           = 4 * 1e-3   # длительность элементарного символа ОК
 
 G_OK_list_str  = list(format(int(read_codes.G_OK_16, 16), '028b'))
 del(G_OK_list_str[25::])
@@ -54,8 +54,8 @@ if int(mod_time /T_ok) == 0:
     num_of_repeat_ok = 1
 else:
     num_of_repeat_ok = int(mod_time /T_ok)
-G_OK_full      = numpy.matlib.repmat(G_OK_array, 1, num_of_repeat_ok)
-
+G_OK_full      = np.transpose(numpy.matlib.repmat(G_OK_array, 1, num_of_repeat_ok))
+G_OK           = np.repeat(G_OK_full, math.ceil(amount_k/ len(G_OK_full)))
 
 """------------------------Цифровые поднесущие-----------------------------"""
 # параметры для формирования
@@ -73,6 +73,7 @@ amount_k_list = [i for i in range(0,amount_k)]
 
 """-----------------------Навигационное сообщение---------------------------"""
 tau_nd = 4 * 1e-3        # длительность одного символа
+
 G_nd_list = []
 for j in range(int(mod_time / tau_nd)):
     if j % 2 == 0: 
@@ -80,15 +81,25 @@ for j in range(int(mod_time / tau_nd)):
     else:
         G_nd_list.append(0)
 G_nd_array = np.array(G_nd_list)
+G_nd_array = np.reshape(G_nd_array ,(5,1))
+G_nd       = np.repeat(G_nd_array, math.ceil(amount_k/ len(G_nd_array)))
 
+
+# цикл для формирования поднесущих и сигнала
 for k in amount_k_list:
-     
+    
+    # определяем номера битов для всех модулирующих последовательностей
+    
+    #N_chip_dk = np.fabs(np.array([math.floor(k/T_dk), len(G_E1_C_array)]))+ 1
+    
+    
     # формируем цифровые поднесущие
     sc_1 = np.sign(math.sin(2* math.pi * R_sc_1 * (k-1) * T_d))
     
     sc_6 = np.sign(math.sin(2* math.pi * R_sc_6 * (k-1) * T_d))
     
-
+    # формируем сигнал
+    S_E1_BC = (A/(math.sqrt(2)))
 
 
 
