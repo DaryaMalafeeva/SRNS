bb41cfebab59ed9523b75959dca5e700a47c5055
Darya Malafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2020-05-05 19:18:29 +0300

    Update main_w3.py

--- main_w3.py
+++ main_w3.py
@@ -15,7 +15,7 @@ f_if     = f_d / 4        # промежуточная частота
 
 mod_time = 20*1e-3        # время моделирования
 
-amount_k = mod_time / T_d # количество отсчетов
+amount_k = int(mod_time / T_d) # количество отсчетов
 
 """---------------------------Дальномерные коды-----------------------------"""
 # параметры дальномерных кодов
@@ -34,6 +34,11 @@ G_E1_C_list_int  = [int(x) for x in G_E1_C_list_str]
 G_E1_C_array     = np.array((G_E1_C_list_int))
 G_E1_C_full      = numpy.matlib.repmat(G_E1_C_array, 1, int(mod_time /T_dk))
 
+
+
+#G_E1_C = np.repeat(G_E1_C_full, math.ceil(amount_k/ len(G_E1_C_full)))
+
+
 """"---------------------------Оверлейный код-------------------------------"""
 # параметры оверлейного кода
 T_ok   = 100 * 1e-3 # период ОК
@@ -49,6 +54,44 @@ if int(mod_time /T_ok) == 0:
     num_of_repeat_ok = 1
 else:
     num_of_repeat_ok = int(mod_time /T_ok)
-    
 G_OK_full      = numpy.matlib.repmat(G_OK_array, 1, num_of_repeat_ok)
+
+
+"""------------------------Цифровые поднесущие-----------------------------"""
+# параметры для формирования
+alpha = math.sqrt(10/11)
+beta  = math.sqrt(1/11)
+
+T_sc_1 = (1/1023) *1e-6  # период sc1
+R_sc_1 = 1 / T_sc_1      # частота sc1
+
+T_sc_6 = (1/6138) *1e-6  # период sc6
+R_sc_6 = 1 / T_sc_6      # частота sc6
+
+
+amount_k_list = [i for i in range(0,amount_k)]
+
+"""-----------------------Навигационное сообщение---------------------------"""
+tau_nd = 4 * 1e-3        # длительность одного символа
+G_nd_list = []
+for j in range(int(mod_time / tau_nd)):
+    if j % 2 == 0: 
+        G_nd_list.append(1)
+    else:
+        G_nd_list.append(0)
+G_nd_array = np.array(G_nd_list)
+
+for k in amount_k_list:
+     
+    # формируем цифровые поднесущие
+    sc_1 = np.sign(math.sin(2* math.pi * R_sc_1 * (k-1) * T_d))
+    
+    sc_6 = np.sign(math.sin(2* math.pi * R_sc_6 * (k-1) * T_d))
+    
+
+
+
+
+
+
 
